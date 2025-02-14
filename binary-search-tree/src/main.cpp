a3b3cfdc4850fee7d29cf10214da3e902c9d1e35
Antonio Natusch <anthony2004951@gmail.com>
2025-02-13 22:36:44 -0400

    feat: make progress in bst methods, todo: insert

--- binary-search-tree/src/main.cpp
+++ binary-search-tree/src/main.cpp
@@ -2,19 +2,33 @@
 
 struct TreeNode {
   int data;
-  TreeNode* left;
-  TreeNode* right;
+  TreeNode *left;
+  TreeNode *right;
 };
 
 class BinarySearchTree {
 public:
+  BinarySearchTree() = default;
+  ~BinarySearchTree() = default;
 
+  BinarySearchTree(TreeNode *r) { this->root = r; }
+
+  TreeNode *Insert(TreeNode *r, int value) {
+
+    if (!r) {
+      r = new TreeNode;
+      r->data = value;
+      return r;
+    }
+
+    return r;
+  }
 
 private:
-  TreeNode* root;
+  TreeNode *root;
 };
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 
   std::cout << "Hello World\n";
 
