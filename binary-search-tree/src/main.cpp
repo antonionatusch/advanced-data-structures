5cffed18df5557eacb97ee4c35c9c2e57c77add6
Antonio Natusch <anthony2004951@gmail.com>
2025-02-13 22:22:56 -0400

    feat(binary-search-tree): add initial main.cpp file

---
+++ binary-search-tree/src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+struct TreeNode {
+  int data;
+  TreeNode* left;
+  TreeNode* right;
+};
+
+class BinarySearchTree {
+public:
+
+
+private:
+  TreeNode* root;
+};
+
+int main (int argc, char *argv[]) {
+
+  std::cout << "Hello World\n";
+
+  return 0;
+}
