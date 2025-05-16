ca36d61adc26956509fb595ae347a6a53feb7270
Antonio Natusch <anthony2004951@gmail.com>
2025-05-16 07:56:06 -0400

    feat(bst): add insert and inorder traversal methods

--- binary-search-tree/src/main.cpp
+++ binary-search-tree/src/main.cpp
@@ -21,7 +21,28 @@ public:
       return r;
     }
 
+    if (r->data == value) {
+      return r;
+    }
+
+    if (r->data > value) {
+      r->left = Insert(r->left, value);
+    } else if (r->data < value) {
+      r->right = Insert(r->right, value);
+    } else {
+      std::cout << "Duplicate value: " << value << std::endl;
+
+      return r;
+    }
     return r;
+  }
+
+  void InOrderTraversal(TreeNode *r) {
+    if (!r)
+      return;
+    InOrderTraversal(r->left);
+    std::cout << r->data << " ";
+    InOrderTraversal(r->right);
   }
 
 private:
@@ -29,8 +50,12 @@ private:
 };
 
 int main(int argc, char *argv[]) {
-
-  std::cout << "Hello World\n";
+  TreeNode* root = new TreeNode{30};
+  auto bst = new BinarySearchTree();
+  bst->Insert(root, 50);
+  bst->Insert(root, 80);
+  bst->Insert(root, 60);
+  bst->InOrderTraversal(root);
 
   return 0;
 }
